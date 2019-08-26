f73ab92ea555e9483004b855c9058e88aee4be9d
Lisa Ngo <lisango.e@gmail.com>
2019-08-26 17:02:32 +0200

    class terminees

---
+++ db/migrate/20190826144606_add_first_name_to_users.rb
@@ -0,0 +1,8 @@
+class AddFirstNameToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :avatar, :string
+    add_column :users, :teacher, :boolean
+  end
+end
