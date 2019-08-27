86251fa61b3a73d600f3fce49be8134143ea4e35
Hector Bonvallot <hector.bonvallot@gmail.com>
2019-08-27 15:14:17 +0200

    d

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2019_08_27_102832) do
     t.string "first_name"
     t.string "last_name"
     t.string "avatar"
-    t.boolean "teacher"
+    t.boolean "teacher", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
