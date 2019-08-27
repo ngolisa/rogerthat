05321371d43e615fc3e0152b03464450629c1a5d
Lisa Ngo <lisango.e@gmail.com>
2019-08-27 10:18:54 +0200

    correction bugg index notins sur la route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coupdroit = Notion.new(name: "Coup droit", description: "test")
