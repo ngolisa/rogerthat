b9a606610e65a01d4d926338bb1e0fc844d6a119
Arthur Marie <arthurmarie1991@gmail.com>
2019-08-29 17:05:09 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,14 +42,28 @@ hector.save
 
 # Notions:
 photo = "https://res.cloudinary.com/dw4g4hgu2/image/upload/v1566997054/2018-07-07-22.21.09_nor9m6.png"
-video = "https://res.cloudinary.com/dw4g4hgu2/video/upload/v1566997867/Roger_Federer_Forehand_Slow_Motion_2019_-_Fluid_Relaxation_hd_pvzjie.mp4"
-
+video = "https://res.cloudinary.com/dw4g4hgu2/video/upload/v1567088445/Coup_droit_muuzjj.mp4"
 coupdroit1 = Notion.create!(
   name: "Le Coup Droit - Comment frapper en avançant",
   user: arthur,
   remote_video_url: video,
   remote_photo_url: photo,
-  description:  "La gestuelle du bras qui frappe la balle vers l'avant est quasi innée. Si c’est le coup fort de la majorité des joueurs de tennis, il n’est pour autant pas maîtrisé techniquement par tout le monde. Nous vous proposons de décortiquer le geste, la prise et la préparation afin d’améliorer votre coup droit et en faire une arme fatale.",
+  description:  "Le coup droit est le coup le plus connu du tennis avec le service. Pourquoi ? Parce qu’il est naturellement effectué par les joueurs qu’ils soient débutants ou experts.
+Tous les joueurs ont la facilité d’effectuer ce coup. Mais avant toute chose, reprenons les bases du coup droit croisé en 5 points clés.
+
+La main libre doit aider à amener la raquette derrière ce qui permet la rotation des épaules.
+
+S’organiser assez vite pour que la fin de préparation se fasse au plus tard au rebond de la balle.
+
+Prise d’appui en ligne, semi-ouvert ou ouvert, en fonction de votre préférence.
+
+Descente de la tête de raquette en dessous du niveau de la balle.
+
+En avant du corps, entre épaules et hanches, et coude décollé du corps (plus vous avez une prise fermée plus le plan de frappe doit être en avant !).
+
+Pour un coup droit croisé, le plan de frappe doit obligatoirement être devant.
+
+Vers le haut et l’avant, le passage du coude doit entraîner le passage de l’épaule et seulement après le passage de l’appui arrière.",
   )
 
 photo = "https://res.cloudinary.com/dw4g4hgu2/image/upload/v1566997159/Nadal-pr_C3_A9paration-modele-de-coup-droit-par-mirsasha_th7gui.jpg"
@@ -77,6 +91,7 @@ Pour un coup droit croisé, le plan de frappe doit obligatoirement être devant.
 Vers le haut et l’avant, le passage du coude doit entraîner le passage de l’épaule et seulement après le passage de l’appui arrière."
   )
 
+video = "https://res.cloudinary.com/dw4g4hgu2/video/upload/v1566997867/Roger_Federer_Forehand_Slow_Motion_2019_-_Fluid_Relaxation_hd_pvzjie.mp4"
 coupdroit3 = Notion.create!(
   name: "Le Coup Droit - Comment se servir des jambes",
   description: "test", user: roger,
@@ -91,7 +106,7 @@ revers1 = Notion.create!(
   user: arthur,
   remote_video_url: video,
   remote_photo_url: photo,
-  description: "Le revers est-il une arme d’attaque ?
+  description: "Le revers comme arme d’attaque
 
 Pour beaucoup de joueurs, il n’est que le coup neutre qui permet au mieux de repousser l’adversaire, au pire celui qui permet de simplement remettre la balle dans le court.
 Pourtant, même si le revers d’attaque est difficile à réaliser, il n’en est pas moins surprenant et peut se révéler une arme redoutable.
@@ -147,9 +162,9 @@ mental1 = Notion.create!(
   )
 
 photo = "https://res.cloudinary.com/dw4g4hgu2/image/upload/v1567069326/102668770_o_dgfolj.jpg"
-video = "https://res.cloudinary.com/dw4g4hgu2/image/upload/v1566998670/roger-federer-mental-conseil-carriere-min_uo30nc.jpg"
+video = "https://res.cloudinary.com/dw4g4hgu2/video/upload/v1567088636/Echauffement_px56el.mp4"
 mental2 = Notion.create!(
-  name: "Le Mental - S'entrainer à penser positif",
+  name: "Avant un match - Comment bien s'échauffer ",
   remote_photo_url: photo,
   description: "test",
   user: roger
